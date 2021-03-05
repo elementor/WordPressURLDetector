6d46a746ee98e9f1643f9bcd168eb62b6fdfd3a7
Leon Stafford <me@ljs.dev>
2021-03-05 14:41:57 +1000

    wip CS fixes, code tidy

---
+++ src/Exception.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * WordPressURLDetectorException.php
+ *
+ * @package           WordPressURLDetector
+ * @author            Leon Stafford <me@ljs.dev>
+ * @license           The Unlicense
+ * @link              https://unlicense.org
+ */
+
+declare(strict_types=1);
+
+namespace WordPressURLDetector;
+
+/**
+ * Throws custom Exception
+ */
+class Exception extends \Exception
+{
+}
